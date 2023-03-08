c6a0958e1f69fe25333fc7a128971f077cfe14fb
jonathan youngblood <jonathan.youngblood@gmail.com>
2023-03-07 19:21:03 -0600

    cool new docs site

---
+++ controllers/docs.php
@@ -0,0 +1,39 @@
+<?php
+
+
+use Slime\render;
+
+
+$app->get('/docs[/]', function ($req, $res, $args) {
+
+  return render::hbs($req, $res, [
+    'layout' => '_layouts/docs',
+    'template' => 'docs/home',
+    'title' => 'docs - ' . $GLOBALS['site_title'],
+    'data' => [
+      'hide_footer' => true
+    ]
+  ]);
+
+});
+
+
+
+
+
+
+
+
+$app->get('/docs/default[/]', function ($req, $res, $args) {
+
+  return render::hbs($req, $res, [
+    'layout' => '_layouts/docs',
+    'template' => 'docs/default',
+    'title' => 'default elements - ' . $GLOBALS['site_title'],
+    'data' => [
+      'hide_footer' => true
+    ]
+  ]);
+
+});
+
--- controllers/index.php
+++ controllers/index.php
@@ -1,36 +1,17 @@
 <?php
 
+require 'controllers/docs.php'; 
+
 
 use Slime\render;
 
-
-
 $app->get('/', function ($req, $res, $args) {
-
   return render::hbs($req, $res, [
     'layout' => '_layouts/base',
     'template' => 'index',
     'title' => $GLOBALS['site_title'],
   ]);
-
 });
-
-
-
-
-
-
-$app->get('/docs[/]', function ($req, $res, $args) {
-
-  return render::hbs($req, $res, [
-    'layout' => '_layouts/base',
-    'template' => 'docs',
-    'title' => $GLOBALS['site_title'],
-  ]);
-
-});
-
-
 
 
 ?>
